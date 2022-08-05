8371ac0a5c3b9ee6548f16237a18270a996c0eeb
Orest-Programmer <85691769+Orest-Programmer@users.noreply.github.com>
2022-08-05 16:19:05 +0300

    Add files via upload
    Fix

--- src/test/java/ua/lviv/lgs/MovieTest.java
+++ src/test/java/ua/lviv/lgs/MovieTest.java
@@ -1,9 +1,40 @@
 package ua.lviv.lgs;
 
+import org.junit.*;
+import org.junit.rules.TestWatcher;
 
 public class MovieTest {
 
 	public Movie movie;
 	
-
+	@Rule
+	public TestWatcher testWathcer = new TestWatcher() {
+		protected void succeed(org.junit.runner.Description description) {
+			System.out.println("Executing " + description.getMethodName() + "...SUCCEED!");
+		};
+		
+		protected void failed(Throwable exception, org.junit.runner.Description description) {
+			System.out.println("Executing " + description.getMethodName() + "...FAILED!");
+		};
+	};
+	
+	@Before
+	public void beforeTest() {
+		Time time = new Time(20,10);
+		movie = new Movie("Test", time);
+	}
+	
+	@After
+	public void afterTest() {
+		movie = null;
+	}
+	
+	@Test
+	public void getTitle() {
+		String realData = movie.getTitle();
+		String expectedValue = "Test";
+//		String expectedValue = "Test2"; Failed
+		Assert.assertEquals(realData, expectedValue);
+	}
+	
 }
